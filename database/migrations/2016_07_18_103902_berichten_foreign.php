20337ae77edbc70a19b71b04974c7e6122c92f75
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2016-07-18 14:41:47 +0200

    Een extra migratie toevoegen die een gelinkte lijst maakt van de berichtentabel

---
+++ database/migrations/2016_07_18_103902_berichten_foreign.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BerichtenForeign extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('berichten', function($table) {
+        
+	  $table->integer("next_id")->unsigned();
+	  $table->integer("previous_id")->unsigned();
+	  
+	  $table->foreign("next_id")->references("id")->on("berichten");
+	  $table->foreign("previous_id")->references("id")->on("berichten");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("berichten", function($table) {
+	  $table->dropForeign("next_id");
+	  $table->dropForeign("previous_id");
+	  
+	  $table->dropColumn("next_id");
+	  $table->dropColumn("previous_id");
+	  
+        });
+    }
+}
