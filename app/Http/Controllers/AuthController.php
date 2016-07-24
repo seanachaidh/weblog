9886c26329e83674fed1825c339a0b85f62e7e2e
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2016-07-24 13:22:42 +0200

    De routes beschermen

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,27 +9,30 @@ use Auth;
 
 class AuthController extends Controller
 {
-    /*
-     * Hier moet ik nog een view weergeven
-     */
-    public function getLogin(Request $request) {
-      return 'Dit is een test';
-    }
+
+	public function getLogin(Request $request) {
+		return 'Dit is een test';
+	}
     
-    public function postLogin(Request $request) {
-      $uname = $request->input('email');
-      $pass = $request->input('password');
+	public function postLogin(Request $request) {
+		$uname = $request->input('email');
+		$pass = $request->input('password');
       
-      $retval = Auth::attempt(['email' => $uname, 'password' => $pass]);
+		$retval = Auth::attempt(['email' => $uname, 'password' => $pass]);
       
-      if($retval) {
-	return redirect('/');
-      } else {
-	return redirect('auth/failed');
-      }
-    }
+		if($retval) {
+			return response()->json(['success' => TRUE]);
+		} else {
+			return response()->json(['success' => FALSE]);
+		}
+	}
     
-    public function getFailed(Request $request) {
-      return view('failedlogin');
-    }
+	public function postCreate(Request $request) {
+		//De code voor het maken van een gebruiker
+	}
+    
+	//TODO Ik moet eens kijken of deze functie wel ergens voor nodig is.
+	public function getFailed(Request $request) {
+		return view('failedlogin');
+	}
 }
