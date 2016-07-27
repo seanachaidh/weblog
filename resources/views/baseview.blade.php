4209f9c614151ac4faf8e2002ec9fa941e33e94d
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2016-07-27 11:46:40 +0200

    Extra views toevoegen

---
+++ resources/views/baseview.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+	
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/mainstyle.css" />
+		<script type="text/javascript" src="javascript/jquery.js"></script>
+		<script type="text/javascript" src="javascript/mainscript.js"></script>
+	</head>
+	
+	<body>
+		@yield('content')
+	</body>
+
+</html>
